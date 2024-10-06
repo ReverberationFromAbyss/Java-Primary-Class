25c1be568d88cd63f66d61cad8a5e6a27ddf916c
dot <1310769510@qq.com>
2024-10-06 13:12:17 +0800

    Feature(Sec2): Data & Variable
    Feature:
    Data&Variable
    String
    
    Further doc:
    Integer
    
    No issues

--- Sec2/src/Test.java
+++ Sec2/src/Test.java
@@ -1,6 +1,9 @@
 public class Test{
   public static void main(String [] args){
-    String str = "1";
-    System.out.println(str.concat("str"));
+String foo = "Hello, World!";
+System.out.println(foo.charAt(1)); // => e
+System.out.println(foo.compareTo("hello, world!"));
+System.out.println(foo.equalsIgnoreCase("hello, world!"));
+System.out.println(foo.indexOf("World"));
   }
 }
